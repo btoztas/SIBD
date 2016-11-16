720bc86516870bb92972b185ee61df1cd007f598
afonsocosta8 <afonside@hotmail.com>
2016-11-16 15:18:00 +0000

    Commented query

--- P2/query.sql
+++ P2/query.sql
@@ -1,7 +1,10 @@
 SELECT doctor_name
-	FROM doctor NATURAL JOIN study
-	WHERE manufacturer = "Philips" AND description LIKE "%X-ray%" AND datediff(current_date, study_date) < 7
-	GROUP BY doctor_id HAVING count(doctor_id) >= ALL (SELECT count(doctor_id)
+	FROM doctor NATURAL JOIN study -- it is needed to have the information on study joined with the information on doctor
+	WHERE manufacturer = "Philips" AND description LIKE "%X-ray%" AND datediff(current_date, study_date) <= 7
+	-- the manufaturer must be Philips, the description must contain the word X-ray and the difference in days between
+	-- the dates must be less or equal to 7;
+	GROUP BY doctor_id HAVING count(doctor_id) >= ALL (SELECT count(doctor_id) -- grouping by doctor_id in order to count 
+							   			   -- the number of studys that a doctor performed
 	FROM study
-	WHERE manufacturer = "Philips" AND description LIKE "%X-ray%" AND datediff(current_date, study_date) < 7
+	WHERE manufacturer = "Philips" AND description LIKE "%X-ray%" AND datediff(current_date, study_date) <= 7
 	GROUP BY doctor_id HAVING count(doctor_id))
