5d373c9a1227817b68e00c83a71a1d15ea9041bd
afonsocosta8 <afonside@hotmail.com>
2016-11-16 15:07:20 +0000

    Started commenting function

--- P2/function.sql
+++ P2/function.sql
@@ -4,14 +4,14 @@ DROP FUNCTION IF EXISTS checkRegion;
 CREATE FUNCTION checkRegion(Ax1 FLOAT(4,3), Ay1 FLOAT(4,3), Ax2 FLOAT(4,3), Ay2 FLOAT(4,3), Bx1 FLOAT(4,3), By1 FLOAT(4,3), Bx2 FLOAT(4,3), By2 FLOAT(4,3))
 RETURNS BOOL
 BEGIN
-        IF Bx1>Bx2 THEN SET Bx1=Bx2+Bx1; SET Bx2 = Bx1-Bx2; SET Bx1= Bx1 - Bx2; END IF;
-        IF Ax1>Ax2 THEN SET Ax1=Ax2+Ax1; SET Ax2 = Ax1-Ax2; SET Ax1= Ax1 - Ax2; END IF;
-        IF By1>By2 THEN SET By1=By2+By1; SET By2 = By1-By2; SET By1= By1 - By2; END IF;
-        IF Ay1>Ay2 THEN SET Ay1=Ay2+Ay1; SET Ay2 = Ay1-Bx2; SET Ay1= Ay1 - Ay2; END IF;
-        IF Bx1 > Ax2 OR By1 > Ay2 OR Ax1 > Bx2 OR Ay1 > By2 THEN
-                RETURN FALSE;
+        IF Bx1>Bx2 THEN SET Bx1=Bx2+Bx1; SET Bx2 = Bx1-Bx2; SET Bx1= Bx1 - Bx2; END IF;-- |
+        IF Ax1>Ax2 THEN SET Ax1=Ax2+Ax1; SET Ax2 = Ax1-Ax2; SET Ax1= Ax1 - Ax2; END IF;-- |->if the values with index 2 are superior
+        IF By1>By2 THEN SET By1=By2+By1; SET By2 = By1-By2; SET By1= By1 - By2; END IF;-- |  to the values with index 1 we must swap 
+        IF Ay1>Ay2 THEN SET Ay1=Ay2+Ay1; SET Ay2 = Ay1-Bx2; SET Ay1= Ay1 - Ay2; END IF;-- |  them
+        IF Bx1 > Ax2 OR By1 > Ay2 OR Ax1 > Bx2 OR Ay1 > By2 THEN -- check if the areas dont overlap
+                RETURN FALSE;  -- if they dont then return false
         ELSE
-                RETURN TRUE;
+                RETURN TRUE;  -- if they overlap return true
         END IF;
 END $$
 
